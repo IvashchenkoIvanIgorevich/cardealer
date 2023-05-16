ca280fb8501cf48633baa0e49ec60139e2859140
Ivan Ivashchenko <ivachko2009@gmail.com>
2023-05-16 15:37:08 +0300

    Added button and model window for creating new car.

--- src/main/java/com/iivan/practice/controller/rest/CarController.java
+++ src/main/java/com/iivan/practice/controller/rest/CarController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/car")
+@RequestMapping("/api/car")
 public class CarController {
 
     private final CarService carService;
